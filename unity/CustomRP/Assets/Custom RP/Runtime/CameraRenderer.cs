366c0bfe2a4fa9382982699e97c5ece25e4d9d78
wanzhiwen <wanzhiwen@joyy.sg>
2021-05-27 16:25:02 +0800

    custom render pipeline

---
+++ unity/CustomRP/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer {
+
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer {
+        name = bufferName
+    };
+    ScriptableRenderContext context;
+    CullingResults cullingResults;
+
+    Camera camera;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera) {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+
+        if (!Cull()) {
+            return;
+        }
+
+        Setup();
+        DrawVisiableGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    bool Cull() {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)) {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+    
+    void DrawVisiableGeometry() {
+        var sortingSettings = new SortingSettings(camera) {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId, sortingSettings
+        );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    void Setup() {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear);
+        buffer.BeginSample(SampleName);
+        ExectueBuffer();
+    }
+
+    void Submit() {
+        buffer.EndSample(SampleName);
+        ExectueBuffer();
+        context.Submit();
+    }
+
+    void ExectueBuffer() {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+}
